83f0f6a40d6df054883714a603a677d0be895f74
omenf1 <owen.lingza@outlook.com>
2023-03-15 14:46:10 +0200

    created a simple home page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,22 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+//  Honestly, I didn't write this navbar example, I took it from another post so that I could focus on getting
+//  all the components working together so that I can get a full view of everything as fast as possible before 
+//  going into finer detail.  This will be the only thig I copy from elsewhere though.
+import NavBar from "../components/navbar";
+
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
+    <>
+      <NavBar />
+      <SessionProvider session={session}>
       <Component {...pageProps} />
-    </SessionProvider>
+      </SessionProvider>
+    </>
   );
 };
 
