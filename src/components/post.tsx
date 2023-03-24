8aa21e329ea3a8cdfe3c89a409f58ef56907632e
omenf1 <owen.lingza@outlook.com>
2023-03-24 16:27:47 +0200

    Quick change of the blog section layout

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link"
 import type {Post} from "../types/postTypes"
 
 type PostProps = {
@@ -10,9 +10,11 @@ export default function PostCard({post} : PostProps) {
     return (
         <>
             <div className="flex flex-col text-white items-center w-full mb-5">
-                <div className="sm:container-sm border-2 border-white rounded-sm w-3/4">
-                    <div className="text-white border-b-2 border-white text-center h-10 bg-blue-500">
-                        <a href={id}>{title}</a>
+                <div className="sm:container-sm border-2 border-white rounded-md w-11/12 sm:w-3/4">
+                    <div className="flex text-white border-b-2 border-white text-center h-10 bg-gray-900 justify-center items-center">
+                        <Link href="#">
+                            {title}
+                        </Link>
                     </div>
                     <div className="text-white h-40 bg-gray-800">
                         {body.substring(0,150)}
